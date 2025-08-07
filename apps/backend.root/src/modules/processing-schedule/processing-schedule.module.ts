caaebce0e5f4188e814c331e3cff77679dabf154
Serhii <avatarcapa@gmail.com>
2025-08-08 01:49:09 +0200

    chore(schedule): add ProcessingScheduleModule with ScheduleModule.forRoot and wire orchestrator/controllers\n\n- Created processing-schedule.module to provide ScheduleOrchestratorService and ExecutionTrackingService\n- Imported module in AppModule for DI and cron support

---
+++ apps/backend.root/src/modules/processing-schedule/processing-schedule.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ProcessingSchedulesController } from './processing-schedule.controller';
+import { EmailAccountsController } from './email-accounts.controller';
+import { ProcessingScheduleService } from './processing-schedule.service';
+import { ScheduleOrchestratorService } from './schedule-orchestrator.service';
+import { ExecutionTrackingService } from './execution-tracking.service';
+import { PrismaModule } from '../../common/prisma/prisma.module';
+import { EmailModule } from '../email/email.module';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    PrismaModule,
+    EmailModule,
+  ],
+  controllers: [ProcessingSchedulesController, EmailAccountsController],
+  providers: [ProcessingScheduleService, ScheduleOrchestratorService, ExecutionTrackingService],
+  exports: [ProcessingScheduleService, ScheduleOrchestratorService, ExecutionTrackingService],
+})
+export class ProcessingScheduleModule {} 
